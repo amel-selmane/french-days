75d085b6c79577e948e90f23b6a6da8d06a3f6f9
amel-selmane <selmane.amel@gmail.com>
2022-03-07 18:51:06 +0100

    Rework countdown + clean HTML

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 // HANDLE COUNTER
-const counterWeeks = document.querySelector('.mainBanner__counter__week'),
-counterDays = document.querySelector('.mainBanner__counter__day'),
-counterMinutes = document.querySelector('.mainBanner__counter__minute'),
-counterSeconds = document.querySelector('.mainBanner__counter__second');
+const counterDays = document.querySelector('.countdown__daysValue'),
+counterHours = document.querySelector('.countdown__hoursValue'),
+counterMinutes = document.querySelector('.countdown__minutesValue'),
+counterSeconds = document.querySelector('.countdown__secondsValue');
 
-setInterval(() => {
-    // TARGET DATE (in english please :)) //
+let countdown = setInterval(() => {
+    // TARGET DATE //
     const limitDate = new Date("April 1, 2022 22:00:00").getTime(),
     // ---------------------------------- //
 
     now = new Date().getTime(),
     timeBetween = limitDate - now,
 
-    weeks = Math.floor(timeBetween / (1000 * 60 * 60 * 24 * 7)),
     days = Math.floor(timeBetween / (1000 * 60 * 60 * 24)),
-    minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60)),
-    seconds = Math.floor((timeBetween % (1000 * 60)) / 1000);
+    hours = Math.floor((timeBetween / (1000 * 60 * 60)) % 24),
+    minutes = Math.floor((timeBetween / (1000 * 60)) % 60),
+    seconds = Math.floor((timeBetween / 1000) % 60);
 
-    counterWeeks.textContent = weeks;
     counterDays.textContent = days;
+    counterHours.textContent = hours;
     counterMinutes.textContent = minutes;
     counterSeconds.textContent = seconds;
+
+    if (timeBetween <= 0) clearInterval(countdown);
 }, 1000)
 
 const buyButton = document.querySelector('.productPush__info__buyButton');
